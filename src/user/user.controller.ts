65b8a63f85900d09ad0a5f48ca45acdb70a48676
siwonpada <psw041115@gmail.com>
2023-04-30 02:23:15 +0900

    Revert "fix: envfile"
    This reverts commit fa5e3f1041b455fc7b74f67929827e912325542b.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Controller('user')
 export class UserController {
